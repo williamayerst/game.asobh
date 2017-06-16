74472ccf93179c7ec2eba03226e516fcd6cc7562
William Ayerst <William.Ayerst@capita.co.uk>
2017-06-16 15:25:59 +0100

    fart2

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 """Basic Module"""
 from pymongo import MongoClient
-
-def confirm(prompt=None, resp=False):
-    """Confirmation Prompt"""
-    if prompt is None:
-        prompt = 'Confirm'
-    if resp:
-        prompt = '%s [%s]|%s: ' % (prompt, 'y', 'n')
-    else:
-        prompt = '%s [%s]|%s: ' % (prompt, 'n', 'y')
-
-    while True:
-        ans = input(prompt)
-        if not ans:
-            return resp
-        if ans not in ['y', 'Y', 'n', 'N']:
-            print('please enter y or n.')
-            continue
-        if ans == 'y' or ans == 'Y':
-            return True
-        if ans == 'n' or ans == 'N':
-            return False
+from bson import ObjectId
 
 def setplayerdetails():
     """Request input for user details"""
@@ -32,7 +12,7 @@ def setplayerdetails():
             print("Not a string!")
             continue
         else:
-            tempdetails["Name"] = name
+            tempdetails["name"] = name
             break
 
     while True:
@@ -64,24 +44,24 @@ class Database:
         self.myuri = MongoClient(uri)
         self.mydata = self.myuri[data]
         self.localdata = self.mydata[collection]
-        # self.localdata = self.mydata.self.mycollection
     def clear(self):
         """Clears data Warband"""
         result = self.localdata.delete_many({})
         print("Cleared Warband of: %d entries" % (result.deleted_count))
     def check_add(self, player):
         """Add player to database"""
-        result = self.localdata.insert_one(player)
-        print(self.localdata.collection_names(include_system_collections=False))
-        print(self.localdata.find())
-    def get(self, _id):
+        self.player = player
+        result = self.localdata.insert_one(self.player).inserted_id
+        print('Added player as ID {id}.'.format(id=result))
+    def get(self, player_id):
         """Function to get record from data:"""
-        document = self.localdata.find_one({'_id': _id})
+        document = self.localdata.find_one({"_id":ObjectId(player_id)})
         print(document)
     def listall(self):
         """Function to get all records from data:"""
-        documentlist = self.localdata.find({})
-        print(documentlist)
+        documentlist = self.localdata.find()
+        for document in documentlist:
+            print(document)
 
 def main():
     """Main Menu Area"""
@@ -89,27 +69,25 @@ def main():
     activedatabase = Database("mongodb://localhost:27017", "asobhdata", "asobhtable")
     while choice == '0':
         print("MENU")
-        print("4. data - Add Figure to Warband")
-        print("5. data - Show Warband")
-        print("6. data - Clear Warband")
+        print("1. Add Figure to Warband")
+        print("2. Show Warband")
+        print("3. Clear Warband")
+        print("4. Get specific player")
         print("9. exit")
         choice = input("Please make a choice: ")
     if choice == "9":
         quit()
-    elif choice == "6":
-        print("data clear")
-        activedatabase = activedatabase.clear
-    elif choice == "5":
-        activedatabase = activedatabase.listall
     elif choice == "4":
-        print("data add")
+        player_id = str(input("Enter ID: "))
+        activedatabase.get(player_id)
+    elif choice == "3":
+        activedatabase.clear()
+    elif choice == "2":
+        activedatabase.listall()
+    elif choice == "1":
         newplayeradd = {}
         newplayeradd = setplayerdetails()
-        for keys,values in newplayeradd.items():
-            print(keys)
-            print(values)
-        
-        # activedatabase = activedatabase.check_add(newplayeradd)
+        activedatabase.check_add(newplayeradd)
     else:
         print("I don't understand your choice.")
 
